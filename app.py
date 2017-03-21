a35ad00b5feb688ad21a5645b843bbc1fa8a448c
Jacksing <hijackyeah@163.com>
2017-03-21 18:32:16 +0800

    Remove xuetangx flags and update page layout

--- app.py
+++ app.py
@@ -24,9 +24,7 @@ HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate, sdch',
     'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4',
-    'Host': 'www.xuetangx.com',
     'Pragma': 'no-cache',
-    'Referer': 'http://www.xuetangx.com/',
     'User-Agent': (
         'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
         ' AppleWebKit/537.36 (KHTML, like Gecko)'
